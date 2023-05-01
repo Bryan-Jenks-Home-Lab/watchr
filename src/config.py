6fb0d424692fe7327dc41c7f49a41d83c913f4d2
Bryan Jenks <bryan@bryanjenks.dev>
2023-05-01 16:32:25 -0700

    👌 IMPROVE: remove config values that are no longer necessary

--- src/config.py
+++ src/config.py
@@ -4,12 +4,6 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     """https://docs.pydantic.dev/usage/settings/"""
 
-    # app_name: str = Field(..., env="APP_NAME")
-    # db_user: str = Field(..., env="DB_USER")
-    # db_password: str = Field(..., env="DB_PASSWORD")
-    # db_server: str = Field(..., env="DB_SERVER")
-    # db_port: str = Field(..., env="DB_PORT")
-    # db_database: str = Field(..., env="DB_DATABASE")
     db_connection_string: str = Field(..., env="DB_CONNECTION_STRING")
 
     watch_path: str = Field(..., env="WATCH_PATH")
