f6fa4134a9f76f527430b0090b2c522cf28647f0
Bryan Jenks <bryan@bryanjenks.dev>
2023-05-05 19:16:16 -0700

    📖 DOC: doc main func

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ load_dotenv()  # Load environment variables from .env file or docker environment
 
 
 def main():
+    """At this point the environmental variables are loaded via `load_dotenv()`
+    and the `MonitorFolder` class is instantiated and started.
+    Environment variables are from either the `.env` file or docker environmental variables.
+    """
     MonitorFolder().start()
 
 
